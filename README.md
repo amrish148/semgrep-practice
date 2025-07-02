e474d78b59590bbf8e8fbaf4f3e459b064e8c03e
amrish148 <53175298+amrish148@users.noreply.github.com>
2025-07-02 10:47:11 +0530

    added semgrep api wrapper
    Api wrapper added over semgrep to respond json for semgrep CLI output

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ java: https://github.com/Rohak-Thakur/Java-MySQL-Connectivity
 c#: https://github.com/kitkav/C-Web-Api
 python spotify: https://github.com/4GeeksAcademy/ginappedrosa-interacting-with-api-python-project-tutorial 
 python DB: https://github.com/imleoos/Python-SQLite-Client
+
+
+curl -X POST http://localhost:8000/scan/ -F "code_path=/Users/amrish/Work/Projects/github/Java-MySQL-Connectivity" -F "config_path=/Users/amrish/Work/Projects/github/semgrep-practice/java-mysql-conn-rules.yaml" 
+
+#Curl output to a json file -o result.json
---
+++ scan_api.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Form
+from fastapi.responses import JSONResponse
+import subprocess
+import os
+
+app = FastAPI()
+
+@app.post("/scan/")
+def scan_local_repo(
+    code_path: str = Form(...), 
+    config_path: str = Form(...)
+):
+    if not os.path.isdir(code_path):
+        return JSONResponse(status_code=400, content={"error": "Invalid code_path"})
+
+    if not os.path.isfile(config_path):
+        return JSONResponse(status_code=400, content={"error": "Invalid config_path"})
+
+    try:
+        result = subprocess.run(
+            ["semgrep", "--json", "--config", config_path, code_path],
+            capture_output=True, text=True, check=True
+        )
+        return JSONResponse(content=result.stdout)
+    except subprocess.CalledProcessError as e:
+        return JSONResponse(status_code=500, content={"error": e.stderr})
